294b1bbdc6c40cf907d401160f8c964f59f1b38f
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-04-01 15:26:59 +0400

    Impementing multiple algorithms

--- Comperator/views.py
+++ Comperator/views.py
@@ -1,5 +1,11 @@
 from flask import render_template
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cluster import KMeans
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.mixture import GaussianMixture
 from Comperator import app
 from Comperator.models import TrainingData, DataCSV
 
@@ -10,12 +16,35 @@ def tain_data_pretty(limit):
     cols, data = dt.get_pretty(limit)
     return render_template('data.html', cols=cols, data=data)
 
-@app.route('/run/knn')
-def run():
+@app.route('/run/<algorithm>')
+def run(algorithm):
     dt = DataCSV()
     features, labels = dt.train_data()
-    clf = KNeighborsClassifier(10)
+    if algorithm == "knn":
+        clf = KNeighborsClassifier(10)
+    elif algorithm == "km":
+        clf = KMeans(2)
+    elif algorithm == "nn":
+        clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                            hidden_layer_sizes=(5, 2), random_state=1)
+    elif algorithm == "svc":
+        clf = SVC()
+    elif algorithm == "gnb":
+        clf = GaussianNB()
+    elif algorithm == "mnb":
+        clf = MultinomialNB()
+    elif algorithm == "bnb":
+        clf = BernoulliNB()
+    elif algorithm == "gm":
+        clf = GaussianMixture()
     clf = clf.fit(features, labels)
     eval_data = dt.eval_data()
     result = clf.predict(eval_data)
-    return render_template('diff.html', data=zip(result, dt.target_data()))
+    zip_lists = zip(result, dt.target_data())
+    diff = sum(1 for i, j in zip_lists if i != j)
+    error = float(diff) / float(len(zip_lists)) * 100.0
+    return render_template('diff.html', 
+                            diff=diff,
+                            length=len(zip_lists),
+                            data=zip_lists, 
+                            error=error)
