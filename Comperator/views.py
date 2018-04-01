b2c213b07a84a422219735615e041c2197141b71
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-04-01 14:51:29 +0400

    Implementing a simple diff with KNN

--- Comperator/views.py
+++ Comperator/views.py
@@ -1,10 +1,21 @@
 from flask import render_template
+from sklearn.neighbors import KNeighborsClassifier
 from Comperator import app
-from Comperator.models import TrainingData
+from Comperator.models import TrainingData, DataCSV
 
 
-@app.route('/data/train')
-def tain_data_pretty():
+@app.route('/data/train/<limit>')
+def tain_data_pretty(limit):
     dt = TrainingData()
-    cols, data = dt.get_pretty()
+    cols, data = dt.get_pretty(limit)
     return render_template('data.html', cols=cols, data=data)
+
+@app.route('/run/knn')
+def run():
+    dt = DataCSV()
+    features, labels = dt.train_data()
+    clf = KNeighborsClassifier(10)
+    clf = clf.fit(features, labels)
+    eval_data = dt.eval_data()
+    result = clf.predict(eval_data)
+    return render_template('diff.html', data=zip(result, dt.target_data()))
