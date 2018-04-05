548fe98ede7d775fd106c7a0e1a074c5fc95e684
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-04-05 17:49:44 +0400

    Preparing for Heroku

--- Comperator/estimate.py
+++ Comperator/estimate.py
@@ -29,8 +29,9 @@ class Estimate():
         return self.stats
 
     def bnb(self, weights):
+        self.params = {'binarize': 1.0, 'alpha': 0.23, 'fit_prior': True}
         self.weights = weights
-        self.clf = BernoulliNB()
+        self.clf = BernoulliNB(**self.params)
         self.clf.fit(self.X, self.y, self.weights)
         self.prediction = self.clf.predict(self.evl)
         self.stats = AnalyseResult(self.prediction, self.tgt).stats()
@@ -53,6 +54,8 @@ class AnalyseResult():
         self.true_negative = len(self.tgt) - self.all_positives - self.false_positive
         self.error_perc = float(self.diff) / float(len(self.zip_lists)) * 100.0
         return {
+            "prediction": self.prediction,
+            "tgt": self.tgt,
             "data": self.zip_lists,
             "accuracy": self.accuracy,
             "diff": self.diff,
@@ -64,3 +67,59 @@ class AnalyseResult():
             "false_negative": self.false_negative,            
         }
            
+class Parameters():
+    def __init__(self,est, alg, grid, weights=None):
+        self.est = est
+        self.alg = alg
+        self.grid = grid
+        self.weights = weights
+
+
+    def get_best_result(self):
+        self.best_precision = {}
+        self.best_precision_score = 0
+        self.best_precision_ac = 0
+        self.best_precision_tp = 0
+        self.best_f1 = {}
+        self.best_f1_score = 0
+        self.best_f1_ac = 0
+        self.best_f1_tp = 0
+        for i in ParameterGrid(self.grid):
+            exec("self.res = self.est.{}(self.weights, i)".format(self.alg))
+            self.tgt = self.res['tgt']
+            self.prediction = self.res['prediction']
+            self.accuracy =  metrics.accuracy_score(self.tgt, self.prediction) * 100.0
+            self.tp = sum(1 for i, j in zip(self.tgt, self.prediction) if i==1.0 and j == 1.0)
+            self.ap = sum(1 for i in self.tgt if i ==1.0)
+            if metrics.f1_score(self.tgt, self.prediction) > self.best_f1_score:
+                self.best_f1_score = metrics.f1_score(self.tgt, self.prediction)
+                self.best_f1 = i
+                self.best_f1_tp = self.tp
+                self.best_f1_ac = self.accuracy
+            if metrics.precision_score(self.tgt, self.prediction) > self.best_precision_score:
+                self.best_precision_score = metrics.precision_score(self.tgt, self.prediction)
+                self.best_precision = i
+                self.best_precision_ac = self.accuracy
+                self.best_precision_tp = self.tp
+            print "###############"
+            print i
+            print "Acuracy: ", self.accuracy
+            print self.tp, "From", self.ap
+            print metrics.precision_score(self.tgt, self.prediction)
+            print metrics.f1_score(self.tgt, self.prediction)
+        print "$$$$$$$$$$$$$"
+        self.dct = {
+            "Best F1 Score": self.best_f1,
+            "With score": self.best_f1_score,
+            "Best F1 TP" : self.best_f1_tp, 
+            "Best F1 Accuracy": self.best_f1_ac,
+            "Best precision Score": self.best_precision,
+            "With score": self.best_precision_score,
+            "Best precision Accuracy": self.best_precision_ac,
+            "Best precision TP": self.best_precision_tp
+        }
+        print "Best F1 Score: ", self.best_f1 
+        print "With score: ", self.best_f1_score
+        print "Best precision Score: ", self.best_precision
+        print "With score: ", self.best_precision_score
+        return self.dct
--- Comperator/views.py
+++ Comperator/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, jsonify
 import numpy as np
 
-from estimate import Estimate
+from estimate import Estimate, Parameters
 from Comperator import app
 from Comperator.models import TrainingData, DataCSV
 from sklearn.model_selection import ParameterGrid
@@ -18,9 +18,27 @@ def tain_data_pretty(limit):
     cols, data = dt.get_pretty(limit)
     return render_template('data.html', cols=cols, data=data)
 
+@app.route('/home')
+
 
 @app.route('/test/<algorithm>')
 def run(algorithm):
+    dt = DataCSV()
+    X, y = dt.train_data()
+    weights = []
+    for i in y:
+        if i == 1.0:
+            weights.append(1)
+        else:
+            weights.append(1)
+    eval_data = dt.eval_data()
+    tgt = dt.target_data()
+    est = Estimate(X, y, eval_data, tgt)
+    exec("stats = est.{}(weights)".format(algorithm))  
+    return render_template('diff.html', **stats)
+
+@app.route('/tune/<algorithm>')
+def tune(algorithm):
     dt = DataCSV()
     X, y = dt.train_data()
     weights = []
@@ -32,44 +50,10 @@ def run(algorithm):
     eval_data = dt.eval_data()
     tgt = dt.target_data()
     est = Estimate(X, y, eval_data, tgt)
-    exec("stats = est.{}(weights)".format(algorithm))
 
-    # gm = list(np.arange(0.1, 1.5, 0.1))
-    # gm.append("auto")
-    # grid = [{'C': np.arange(0.1, 4, 0.1), 'kernel': ['rbf'],'gamma':gm}]
-        
-    # elif algorithm == 'gnb':
-    #     clf = GaussianNB()
-    #     clf.fit(X, y, weights)        
-    # elif algorithm == 'bnb':
-    #     clf = BernoulliNB()
-    #     clf.fit(X, y, weights)        
-    # elif algorithm == 'mnb':
-    #     clf = MultinomialNB()
-    #     clf.fit(X, y, weights)        
-    
-    
-    return render_template('diff.html', **stats)
-    
-    # best = {}
-    # best_score = 0
-    # for i in ParameterGrid(grid):
-    #     clf = SVC(**i)
-    #     clf.fit(X, y, weights)
-    #     prediction = clf.predict(eval_data)
-    #     ac =  metrics.accuracy_score(tgt, prediction) * 100.0
-    #     cor = sum(1 for i, j in zip(tgt, prediction) if i==1.0 and j == 1.0)
-    #     al = sum(1 for i in tgt if i ==1.0)
-    #     if metrics.f1_score(tgt, prediction) > best_score:
-    #         best_score = metrics.f1_score(tgt, prediction)
-    #         best = i
-    #     print "###############"
-    #     print i
-    #     print cor, "From", al
-    #     print "Acuracy: ", ac
-    #     print metrics.accuracy_score(tgt, prediction)
-    #     print metrics.precision_score(tgt, prediction)
-    #     print metrics.f1_score(tgt, prediction)
-    # print "$$$$$$$$$$$$$"
-    # print best , best_score
-
+    gm = list(np.arange(0.01, 3.0, 0.01))
+    gm.append(None)
+    gm.append(0.0)
+    grid = [{'alpha': np.arange(0.01, 3, 0.01), 'fit_prior': [True,False], 'binarize': gm}]
+    params_test = Parameters(est=est, alg='bnb', grid=grid, weights=weights)
+    return jsonify(params_test.get_best_result())
